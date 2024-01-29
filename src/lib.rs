80b0ed16731debd665dc573f24a00a18428b79e4
pepedeulloa <lucascounhagobarreiro@gmail.com>
2024-01-29 16:43:57 +0100

    Refactored the lib.rs file to improve code structure and readability. Introduced a default option. Implemented the "output" function to neatly display counts. Implemented a specific function to calculate the total count of all files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod config {
- use std::{ path::PathBuf, io::{BufReader, BufRead}, error::Error, fs::File };
+ use std::{ error::Error, fs::File, io::{BufReader, BufRead}, path::PathBuf };
 
  use clap::Parser;
  
@@ -11,11 +11,11 @@ pub mod config {
   pub files: Vec<PathBuf>,
  
   /// Print bytes count of the file
-  #[arg(short='c', long="bytes", default_value_t=true)]
+  #[arg(short='c', long="bytes", default_value_t=false)]
   pub bytes: bool,
  
   /// Print lines count of the file
-  #[arg(short='l', long="lines", default_value_t=true)]
+  #[arg(short='l', long="lines", default_value_t=false)]
   pub lines: bool,
  
   /// Print character count of the file
@@ -23,10 +23,18 @@ pub mod config {
   pub chars: bool,
  
   /// Print words count of the file
-  #[arg(short='w', long="words", default_value_t=true)]
+  #[arg(short='w', long="words", default_value_t=false)]
   pub words: bool,
  
  }
+
+	pub fn is_default(bytes: bool, chars: bool, lines: bool, words: bool) -> bool {
+		if !bytes && !chars && !lines && !words {
+			true
+		} else {
+			false
+		}
+	}
  
  pub struct FileCount {
   pub bytes: usize,
@@ -37,28 +45,44 @@ pub mod config {
  
   pub words: usize,
  }
+
+	impl FileCount {
+		pub fn new() -> Self {
+			FileCount {
+				bytes: 0, 
+				lines: 0, 
+				chars: 0, 
+				words: 0
+			}
+		}
+
+		pub fn increment_counters(&mut self, file_count: FileCount) {
+			self.bytes += file_count.bytes;
+			self.lines += file_count.lines;
+			self.chars += file_count.chars;
+			self.words += file_count.words;
+		}
+	}
  
  pub fn get_args() -> Result<Cli, Box<dyn Error>> {
- 
   let cli = Cli::parse();
- 
   Ok(cli)
  }
  
- pub fn open(filename: PathBuf) -> Result<BufReader<File>, Box<dyn std::error::Error>> {
+ fn open(filename: PathBuf) -> Result<BufReader<File>, Box<dyn std::error::Error>> {
   let file = File::open(filename)?;
   let reader = BufReader::new(file);
   
   Ok(reader)
  }
  
-fn count_bytes (line: &String) -> Result<usize, Box<dyn std::error::Error>>  {
-  let count = line.chars().count();
+	fn count_bytes (line: &String) -> Result<usize, Box<dyn std::error::Error>>  {
+  let count = line.bytes().count();
   Ok(count)
  }
 
-fn count_chars (line: &String) -> Result<usize, Box<dyn std::error::Error>> {
-  let count = line.bytes().count();
+ fn count_chars (line: &String) -> Result<usize, Box<dyn std::error::Error>> {
+  let count = line.chars().count();
   Ok(count)
  }
 
@@ -66,15 +90,24 @@ fn count_chars (line: &String) -> Result<usize, Box<dyn std::error::Error>> {
   Ok(line.split_whitespace().count())
  }
 
- pub fn run_file(reader: BufReader<File>, bytes: bool, lines: bool, chars: bool, words: bool ) -> Result<FileCount,Box <dyn std::error::Error>> {
-  let mut file_count = FileCount{
-   bytes : 0,
-   lines : 0,
-   chars : 0,
-   words : 0
-  };
+ pub fn run_file(file: PathBuf, default: bool, bytes: bool, lines: bool, chars: bool, words: bool ) -> Result<FileCount,Box <dyn std::error::Error>> {
+		let reader = open(file).unwrap();
+		
+		let mut file_count = FileCount{
+			bytes : 0,
+			lines : 0,
+			chars : 0,
+			words : 0
+		};
 
   for line in reader.lines(){
+
+			if default {
+				file_count.lines += 1;
+				file_count.words += count_words(line.as_ref().unwrap()).unwrap();
+				file_count.bytes += count_bytes(line.as_ref().unwrap()).unwrap();
+			}
+
    if bytes {
     file_count.bytes += count_bytes(line.as_ref().unwrap()).unwrap();
    }
@@ -88,6 +121,40 @@ fn count_chars (line: &String) -> Result<usize, Box<dyn std::error::Error>> {
     file_count.lines += 1;
    }
   }
-  Ok(file_count)
+		Ok(file_count)
  }
+
+	pub fn output(f: &FileCount, name: &str, default: bool, bytes: bool, lines: bool, chars: bool, words: bool) {
+		if default {
+			print!("\t{}\t{}\t{}\t{}\n", f.lines, f.words, f.bytes, name);
+		}
+		if lines {
+			print!("\t{}", f.lines);
+		}
+		if bytes {
+			print!("\t{}", f.bytes);
+		}
+		if chars {
+			print!("\t{}", f.chars);
+		}
+		if words {
+			print!("\t{}", f.words);
+		}
+		if !default {
+			print!("\t{}\n", name);
+		}
+	}
+
+	pub fn total(vector: Vec<FileCount>) -> Result<FileCount, Box <dyn std::error::Error>> {
+		let mut file_count = FileCount{
+			bytes : 0,
+   lines : 0,
+   chars : 0,
+   words : 0
+		};
+		for el in vector  {
+			file_count.increment_counters(el)
+		}
+		Ok(file_count)
+	}
 } 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use wc_rust::config::{ Cli, get_args, open, run_file, FileCount};
+use wc_rust::config::{ get_args, is_default, output, run_file, total, Cli, FileCount};
 
 fn main() {
     let Cli{files, bytes, lines, chars, words} = get_args().unwrap();
 
-    for f in files {
-        let name = f.clone();
-        let reader = open(f).unwrap();
-        let FileCount {bytes, lines, words, ..}: FileCount = run_file(reader, bytes, lines, chars, words).unwrap();
-        println!("\tlines:{:?}\twords:{:?}\tbytes:{:?} {:?}", lines, words, bytes, name )
+    let num_files: usize = files.len();
+    let mut file_count_vec: Vec<FileCount> = Vec::new();
+    
+    if num_files > 1 {
+        for f in files {
+            let file_count: FileCount = run_file(f.clone(), is_default(bytes, chars, lines, words), bytes, lines, chars, words).unwrap(); 
+            output(&file_count, f.clone().to_str().unwrap(), is_default(bytes, chars, lines, words), bytes, lines, chars, words);
+            file_count_vec.push(file_count);
+        }
+        let total: FileCount = total(file_count_vec).unwrap();
+        output(&total, "total", is_default(bytes, chars, lines, words), bytes, lines, chars, words)
+    } else {
+        let file_count: FileCount = run_file(files[0].clone(), is_default(bytes, chars, lines, words), bytes, lines, chars, words).unwrap();
+        output(&file_count, files[0].clone().to_str().unwrap(), is_default(bytes, chars, lines, words), bytes, lines, chars, words);
     }
-
 }
